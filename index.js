e9819bc3a7b042755aaa02d9d1cf68621dc2dc75
Viviana <viviana.perez28@gmail.com>
2024-08-08 15:08:12 -0500

    change-port

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ app.post('/gateway', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
