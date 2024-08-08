7e23163c5615c8b0b386fc93dd1da51107b475bb
Viviana <viviana.perez28@gmail.com>
2024-08-08 17:35:36 -0500

    get-test

--- index.js
+++ index.js
@@ -8,18 +8,36 @@ app.post('/gateway', async (req, res) => {
     console.log('Request body:', req.body); // Log para verificar el cuerpo de la solicitud
 
     try {
-        // Realiza la solicitud a la API externa
-        const response = await axios.post('https://api.preveo.bocetos.co/auth/keycloak/login', {
+        // Realiza la solicitud a la API externa para obtener el token
+        const tokenResponse = await axios.post('https://api.preveo.bocetos.co/auth/keycloak/login', {
             username: req.body.username,
             password: req.body.password
         });
 
-        console.log('API response data:', response.data); // Log para verificar los datos de la API
+        console.log('Token response data:', tokenResponse.data); // Log para verificar los datos del token
 
-        res.json({ accessToken: response.data.access_token });
+        const token = tokenResponse.data.data.access_token;
+
+        // if (!token) {
+        //     return res.status(500).send('Error en la autenticación: Token no recibido');
+        // }
+
+        // Realiza la solicitud GET usando el token obtenido
+        const dataResponse = await axios.get('https://api.preveo.bocetos.co/vg16get_info_update/beeff9c2-ae1e-4fad-8265-26030bb89c9d', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        console.log('Data response status:', dataResponse.status);
+        console.log('Data response data:', dataResponse.data);
+
+        // Enviar los datos obtenidos como respuesta
+        res.json(dataResponse.data);
+
     } catch (error) {
         console.error('Error during API request:', error.message); // Log del error
-        res.status(500).send('Error en la autenticación xd');
+        res.status(500).send('Error al procesar la solicitud');
     }
 });
 
